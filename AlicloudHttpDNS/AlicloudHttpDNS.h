0ce1c1d9808786eb1e30ded081f790bc17647e4b
地风 <elonchan.cyl@alibaba-inc.com>
2018-06-25 14:56:45 +0800

    fix bug for network change

--- AlicloudHttpDNS/AlicloudHttpDNS.h
+++ AlicloudHttpDNS/AlicloudHttpDNS.h
@@ -19,7 +19,7 @@
 
 #import <Foundation/Foundation.h>
 
-static NSString *const HTTPDNS_IOS_SDK_VERSION = @"1.6.14";
+static NSString *const HTTPDNS_IOS_SDK_VERSION = @"1.6.16";
 
 #import <Foundation/Foundation.h>
 #import "HttpdnsServiceProvider.h"
