3b3d7d930c1444113e5424c0515858f63806e850
lingkun.lk <lingkun.lk@alibaba-inc.com>
2018-11-15 19:14:03 +0800

    修改v6结果存储逻辑

--- AlicloudHttpDNS/ipv6/HttpdnsIPv6Manager.m
+++ AlicloudHttpDNS/ipv6/HttpdnsIPv6Manager.m
@@ -8,21 +8,12 @@
 
 #import <AlicloudUtils/AlicloudIPv6Adapter.h>
 #import <AlicloudUtils/EMASTools.h>
-#import "HttpdnsPersistenceUtils.h"
-#import "HttpdnsScheduleCenter.h"
 #import "HttpdnsIPv6Manager.h"
 #import "HttpDnsHitService.h"
 
 @interface HttpdnsIPv6Manager()
 
 @property (nonatomic, assign) BOOL usersetIPv6ResultEnable;
-@property (nonatomic, assign) BOOL usersetIPv6ServiceEnable;
-@property (nonatomic, assign) BOOL ipv6ServiceEnable;
-@property (nonatomic, assign) BOOL ipv6ServiceMask;
-@property (nonatomic, copy) NSString *ipv6ServiceStopFlagPath;
-@property (nonatomic, strong) dispatch_queue_t syncQueue;
-@property (nonatomic, strong) dispatch_queue_t storeSyncQueue;
-@property (nonatomic, strong) NSMutableDictionary *storeIPv6Results;
 
 @end
 
@@ -31,12 +22,6 @@
 - (instancetype)init {
     if (self = [super init]) {
         _usersetIPv6ResultEnable = NO;
-        _usersetIPv6ServiceEnable = NO;
-        _ipv6ServiceEnable = NO;
-        _ipv6ServiceMask = NO;
-        _syncQueue = dispatch_queue_create("com.httpdns.ipv6.manager", NULL);
-        _storeSyncQueue = dispatch_queue_create("com.httpdns.ipv6.store", NULL);
-        _storeIPv6Results = [NSMutableDictionary dictionary];
     }
     return self;
 }
@@ -75,37 +60,6 @@
 
 - (BOOL)isAbleToResolveIPv6Result {
     return _usersetIPv6ResultEnable;
-}
-
-- (void)storeIPv6ResolveRes:(NSArray<HttpdnsIpObject *> *)ipv6Array forHost:(NSString *)host {
-    if (![EMASTools isValidString:host] || ![EMASTools isValidArray:ipv6Array]) {
-        return ;
-    }
-    dispatch_sync(_storeSyncQueue, ^{
-        [_storeIPv6Results setObject:ipv6Array forKey:host];
-    });
-}
-
-- (NSArray<HttpdnsIpObject *> *)getIPv6ObjectArrayForHost:(NSString *)host {
-    __block NSArray *res = nil;
-    dispatch_sync(_storeSyncQueue, ^{
-        res = [_storeIPv6Results objectForKey:host];
-    });
-    return res;
-}
-
-- (NSArray<NSString *> *)getIP6StringsByHost:(NSString *)host {
-    NSArray<HttpdnsIpObject *> *ipRecords = [self getIPv6ObjectArrayForHost:host];
-    if (!ipRecords) {
-        return nil;
-    }
-    NSMutableArray<NSString *> *ip6s = [NSMutableArray arrayWithCapacity:ipRecords.count];
-    for (HttpdnsIpObject *ipObject in ipRecords) {
-        @try {
-            [ip6s addObject:ipObject.ip];
-        } @catch (NSException *exception) {}
-    }
-    return [ip6s copy];
 }
 
 @end
