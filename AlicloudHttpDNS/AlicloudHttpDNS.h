fcf2c8d39f8f554c3feb13610856ac6de4255e02
lingkun.lk <lingkun.lk@alibaba-inc.com>
2019-03-08 15:01:09 +0800

    Update to v1.7.0_RC_02

--- AlicloudHttpDNS/AlicloudHttpDNS.h
+++ AlicloudHttpDNS/AlicloudHttpDNS.h
@@ -19,7 +19,7 @@
 
 #import <Foundation/Foundation.h>
 
-static NSString *const HTTPDNS_IOS_SDK_VERSION = @"1.7.0";
+static NSString *const HTTPDNS_IOS_SDK_VERSION = @"1.7.0_RC_02";
 
 #import <Foundation/Foundation.h>
 #import "HttpdnsLoggerDelegate.h"
