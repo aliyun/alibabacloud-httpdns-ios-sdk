2252b2eca442f84114b265e8f15542ce40ed78aa
地风 <elonchan.cyl@alibaba-inc.com>
2019-03-20 16:30:17 +0800

    update version

--- AlicloudHttpDNS/AlicloudHttpDNS.h
+++ AlicloudHttpDNS/AlicloudHttpDNS.h
@@ -19,7 +19,7 @@
 
 #import <Foundation/Foundation.h>
 
-static NSString *const HTTPDNS_IOS_SDK_VERSION = @"1.7.1";
+static NSString *const HTTPDNS_IOS_SDK_VERSION = @"1.7.2";
 
 #import <Foundation/Foundation.h>
 #import "HttpdnsLoggerDelegate.h"
