91dd964dd85198da8df7cd4c22c79af75710881d
魏晓堃 <wb-wxk416923@alibaba-inc.com>
2020-05-28 10:20:47 +0800

    修改版本号 1.19.2.4

--- AlicloudHttpDNS/AlicloudHttpDNS.h
+++ AlicloudHttpDNS/AlicloudHttpDNS.h
@@ -19,7 +19,7 @@
 
 #import <Foundation/Foundation.h>
 
-static NSString *const HTTPDNS_IOS_SDK_VERSION = @"1.19.2.3";
+static NSString *const HTTPDNS_IOS_SDK_VERSION = @"1.19.2.4";
 
 #import <Foundation/Foundation.h>
 #import "HttpdnsLoggerDelegate.h"
