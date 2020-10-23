bfa1742d21410af4a609015360d831391b61f064
xiangji.bxj <xiangji.bxj@alibaba-inc.com>
2020-10-23 11:46:39 +0800

    feat:修改版本号至1.19.2.8

--- AlicloudHttpDNS/AlicloudHttpDNS.h
+++ AlicloudHttpDNS/AlicloudHttpDNS.h
@@ -19,7 +19,7 @@
 
 #import <Foundation/Foundation.h>
 
-static NSString *const HTTPDNS_IOS_SDK_VERSION = @"1.19.2.7";
+static NSString *const HTTPDNS_IOS_SDK_VERSION = @"1.19.2.8";
 
 #import <Foundation/Foundation.h>
 #import "HttpdnsLoggerDelegate.h"
