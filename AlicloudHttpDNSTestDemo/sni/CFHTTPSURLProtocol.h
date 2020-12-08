c904c54aeaca39c6d7289e9b90b643832b1aa3f9
yannan <yannan.cr@alibaba-inc.com>
2020-12-08 17:28:24 +0800

    feat:  增加sni protocol

---
+++ AlicloudHttpDNSTestDemo/sni/CFHTTPSURLProtocol.h
@@ -0,0 +1,13 @@
+//
+//  CFHTTPSURLProtocol.h
+//  BDHttpDnsSDKDemo
+//
+//  Created by yannan on 2020/12/8.
+//  Copyright © 2020 alibaba-inc.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CFHTTPSURLProtocol : NSURLProtocol
+
+@end
