3d1618edf89d83a644fc2b2118b1b5eff242e883
yannan <yannan.cr@alibaba-inc.com>
2022-01-16 17:06:35 +0800

    feat:  解析兜底逻辑修改，在有region场景不走兜底IP

--- AlicloudHttpDNS/HttpdnsRequest.m
+++ AlicloudHttpDNS/HttpdnsRequest.m
@@ -253,7 +253,7 @@ static NSURLSession *_resolveHOSTSession = nil;
     NSString *serverIp = [scheduleCenter getActivatedServerIPWithIndex:activatedServerIPIndex];
     self.serviceRegion = [scheduleCenter getServiceIPRegion]; //获取当前service IP 的region
     
-    if ([EMASTools isValidString:self.serviceRegion] && [@[ALICLOUD_HTTPDNS_SERVER_IP_ACTIVATED, ALICLOUD_HTTPDNS_SCHEDULE_CENTER_REQUEST_HOST_IP, ALICLOUD_HTTPDNS_SCHEDULE_CENTER_REQUEST_HOST_IP_2] containsObject:serverIp]) { //如果当前设置region 并且 当次服务IP是国内兜底IP 则直接禁止解析行为
+    if ([EMASTools isValidString:self.serviceRegion] && [@[ALICLOUD_HTTPDNS_DEFAULT_SERVER_IP, ALICLOUD_HTTPDNS_SCHEDULE_CENTER_REQUEST_HOST_IP, ALICLOUD_HTTPDNS_SCHEDULE_CENTER_REQUEST_HOST_IP_2] containsObject:serverIp]) { //如果当前设置region 并且 当次服务IP是国内兜底IP 则直接禁止解析行为
         return nil;
     }
     
