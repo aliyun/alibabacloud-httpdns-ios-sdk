4ca45a0bbf220a3dd4abe4c0ef9c8199703c871f
瑾笙 <allen.wy@alibaba-inc.com>
2023-01-17 11:42:40 +0800

    feat: build.sh删除i386架构

--- AlicloudHttpDNS/AlicloudHttpDNS.h
+++ AlicloudHttpDNS/AlicloudHttpDNS.h
@@ -19,7 +19,7 @@
 
 #import <Foundation/Foundation.h>
 
-static NSString *const HTTPDNS_IOS_SDK_VERSION = @"2.0.10";
+static NSString *const HTTPDNS_IOS_SDK_VERSION = @"2.0.9";
 
 #import <Foundation/Foundation.h>
 #import "HttpdnsLoggerDelegate.h"
