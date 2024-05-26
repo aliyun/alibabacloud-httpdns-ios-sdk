314c2f51f1be9f3cf8fd95870d5f72b4d9b9d580
周卓 <yecan.xyc@alibaba-inc.com>
2024-05-26 15:38:56 +0800

    feat: 增加针对只合并了单类型缓存却解析双类型的单例case

--- AlicloudHttpDNSTests/HighLevelTest/PresetCacheAndRetrieveTest.m
+++ AlicloudHttpDNSTests/HighLevelTest/PresetCacheAndRetrieveTest.m
@@ -127,7 +127,7 @@ static NSMutableArray *mockedObjects;
 - (void)shouldNotHaveCalledRequestWhenResolving:(void (^)(void))resolvingBlock {
     HttpdnsRequestScheduler *scheduler = self.httpdns.requestScheduler;
     HttpdnsRequestScheduler *mockScheduler = OCMPartialMock(scheduler);
-    OCMReject([mockScheduler executeRequest:[OCMArg any] retryCount:0 activatedServerIPIndex:0 error:[OCMArg any]]).andReturn(nil);
+    OCMReject([mockScheduler executeRequest:[OCMArg any] retryCount:0 activatedServerIPIndex:0 error:[OCMArg any]]);
     resolvingBlock();
     OCMVerifyAll(mockScheduler);
     [mockedObjects addObject:mockScheduler];
@@ -201,6 +201,69 @@ static NSMutableArray *mockedObjects;
     XCTAssertTrue([result.ips[0] isEqualToString:ipv41]);
     XCTAssertTrue([result.ipv6s count] == 2);
     XCTAssertTrue([result.ipv6s[0] isEqualToString:ipv61]);
+}
+
+// 只缓存ipv4单栈的地址，按请求双栈类型存入，此时会标记该域名没有ipv6地址
+// 按预期，会判断该域名没有ipv6地址，因此不会返回ipv6地址，也不会发请求
+- (void)testMergeNoIpv6ResultAndGetBoth {
+    [self presetNetworkEnvAsIpv4AndIpv6];
+
+    HttpdnsHostObject *hostObject = [self constructSimpleIpv4HostObject];
+    // 双栈下解析结果仅有ipv4，合并时会标记该host无ipv6
+    [self.httpdns.requestScheduler mergeLookupResultToManager:hostObject host:ipv4OnlyHost cacheKey:ipv4OnlyHost underQueryIpType:HttpdnsQueryIPTypeBoth];
+
+    // 使用同步接口，要切换到异步线程，否则内部会自己切
+    [self shouldNotHaveCalledRequestWhenResolving:^{
+        HttpdnsResult *result = [self.httpdns resolveHostSync:ipv4OnlyHost byIpType:HttpdnsQueryIPTypeBoth];
+        XCTAssertNotNil(result);
+        XCTAssertTrue([result.host isEqualToString:ipv4OnlyHost]);
+        XCTAssertTrue([result.ips count] == 2);
+        XCTAssertTrue([result.ips[0] isEqualToString:ipv41]);
+    }];
+}
+
+// 缓存ipv4单栈的地址，但是请求ipv4类型存入，此时不会打标记没有ipv6
+// 于是，读取时，会尝试发请求获取ipv6地址
+- (void)testMergeOnlyIpv4ResultAndGetBoth {
+    [self presetNetworkEnvAsIpv4AndIpv6];
+
+    HttpdnsHostObject *hostObject = [self constructSimpleIpv4HostObject];
+    [self.httpdns.requestScheduler mergeLookupResultToManager:hostObject host:ipv4OnlyHost cacheKey:ipv4OnlyHost underQueryIpType:HttpdnsQueryIPTypeIpv4];
+
+    dispatch_semaphore_t sema = dispatch_semaphore_create(0);
+
+    // 使用同步接口，要切换到异步线程，否则内部会自己切
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        [self shouldHaveCalledRequestWhenResolving:^{
+            HttpdnsResult *result = [self.httpdns resolveHostSync:ipv4OnlyHost byIpType:HttpdnsQueryIPTypeBoth];
+            XCTAssertNil(result);
+            dispatch_semaphore_signal(sema);
+        }];
+    });
+
+    dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
+}
+
+// 缓存ipv6单栈的地址，但是请求ipv6类型存入，此时不会打标记没有ipv4
+// 于是，读取时，会尝试发请求获取ipv4地址
+- (void)testMergeOnlyIpv6ResultAndGetBoth {
+    [self presetNetworkEnvAsIpv4AndIpv6];
+
+    HttpdnsHostObject *hostObject = [self constructSimpleIpv6HostObject];
+    [self.httpdns.requestScheduler mergeLookupResultToManager:hostObject host:ipv6OnlyHost cacheKey:ipv6OnlyHost underQueryIpType:HttpdnsQueryIPTypeIpv6];
+
+    dispatch_semaphore_t sema = dispatch_semaphore_create(0);
+
+    // 使用同步接口，要切换到异步线程，否则内部会自己切
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        [self shouldHaveCalledRequestWhenResolving:^{
+            HttpdnsResult *result = [self.httpdns resolveHostSync:ipv6OnlyHost byIpType:HttpdnsQueryIPTypeBoth];
+            XCTAssertNil(result);
+            dispatch_semaphore_signal(sema);
+        }];
+    });
+
+    dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
 }
 
 @end
