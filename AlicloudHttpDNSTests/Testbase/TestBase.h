6833de136ee55f3f4aafe813852e588e35708eee
周卓 <yecan.xyc@alibaba-inc.com>
2024-05-27 10:11:15 +0800

    refact: 把单元测试中可以复用的部分抽取到基类

--- AlicloudHttpDNSTests/Testbase/TestBase.h
+++ AlicloudHttpDNSTests/Testbase/TestBase.h
@@ -7,8 +7,16 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AlicloudUtils/AlicloudUtils.h>
 #import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
 #import "XCTestCase+AsyncTesting.h"
+#import "HttpdnsRequest.h"
+#import "HttpdnsHostObject.h"
+#import "HttpdnsRequestScheduler_Internal.h"
+#import "HttpdnsService.h"
+#import "HttpdnsService_Internal.h"
+
 
 #define NOTIFY [self notify:XCTAsyncTestCaseStatusSucceeded];
 #define WAIT [self waitForStatus:XCTAsyncTestCaseStatusSucceeded timeout:30];
@@ -17,6 +25,35 @@
 #define WAIT_10 [self waitForStatus:XCTAsyncTestCaseStatusSucceeded timeout:10.0];
 #define WAIT_FOREVER [self waitForStatus:XCTAsyncTestCaseStatusSucceeded timeout:DBL_MAX];
 
+static NSString *ipv4OnlyHost = @"ipv4.only.com";
+static NSString *ipv6OnlyHost = @"ipv6.only.com";
+static NSString *ipv4AndIpv6Host = @"ipv4.and.ipv6.com";
+
+static NSString *ipv41 = @"1.1.1.1";
+static NSString *ipv42 = @"2.2.2.2";
+static NSString *ipv61 = @"2001:4860:4860::8888";
+static NSString *ipv62 = @"2001:4860:4860::8844";
+
+static NSMutableArray *mockedObjects;
+
 @interface TestBase : XCTestCase
+
+@property (nonatomic, assign) NSTimeInterval currentTimeStamp;
+
+- (HttpdnsHostObject *)constructSimpleIpv4HostObject;
+
+- (HttpdnsHostObject *)constructSimpleIpv6HostObject;
+
+- (HttpdnsHostObject *)constructSimpleIpv4AndIpv6HostObject;
+
+- (void)presetNetworkEnvAsIpv4;
+
+- (void)presetNetworkEnvAsIpv6;
+
+- (void)presetNetworkEnvAsIpv4AndIpv6;
+
+- (void)shouldNotHaveCalledRequestWhenResolving:(void (^)(void))resolvingBlock;
+
+- (void)shouldHaveCalledRequestWhenResolving:(void (^)(void))resolvingBlock;
 
 @end
